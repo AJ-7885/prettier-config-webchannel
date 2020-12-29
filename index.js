00c1eac6acfcd427f970b86af9d61691f50a116f
AJ <42003122+AJ-7885@users.noreply.github.com>
2020-12-29 18:28:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,37 @@
   "singleQuote": true,
   "trailingComma": "all",
   "endOfLine": "lf"
+  
+  
+  
+
+  "embeddedLanguageFormatting": "auto",
+  "htmlWhitespaceSensitivity": "css",
+  "insertPragma": false,
+
+
+
+  "proseWrap": "preserve",
+  "quoteProps": "as-needed",
+  "requirePragma": false,
+
+
+
+
+  "useTabs": false,
+  "vueIndentScriptAndStyle": false
+    trailingComma: 'es5',
+        singleQuote: true,
+        jsxSingleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        arrowParens: 'avoid',
+        printWidth: 80,
+        // below line only for windows users facing CLRF and eslint/prettier error
+        // non windows users feel free to delete it
+        endOfLine: 'auto',
+  
+  
 }
